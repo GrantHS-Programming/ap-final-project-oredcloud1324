7903ed01f4a73ab8717c2eca800119e36bd66561
oredcloud1324 <oredcloud1324@student.pps.net>
2024-05-23 11:52:28 -0700

    4

--- Assets/SMOOTHCAMRAFOLLOW.cs
+++ Assets/SMOOTHCAMRAFOLLOW.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class SMOOTHCAMRAFOLLOW : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[SerializeField] private Vector3 offset;
+[SerializeField] private float damping;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public Transform target;
+
+private Vector3 vel = Vector3.zero;
+
+private void FixedUpdate() {
+Vector3 targetPosition = target.position + offset;
+targetPosition.z = transform.position.z;
+
+transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref vel, damping);
+}
 }
